fc7db7785eeefa52d6eac79a83c1d84d0bf38cf4
azbw2 <199152344+azbw2@users.noreply.github.com>
2025-07-17 14:20:54 -0700

    Bump all dependency versions

--- test/AwsmskAuthTokenGeneratorAsyncTest.cs
+++ test/AwsmskAuthTokenGeneratorAsyncTest.cs
@@ -1,60 +1,157 @@
 ﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using Amazon;
-using Amazon.Runtime;
-using Moq;
 using System.Globalization;
 using System.Text;
-using System.Web;
 using System.Text.RegularExpressions;
+using System.Web;
+using Amazon;
+using Amazon.Runtime;
 using Amazon.SecurityToken;
 using Amazon.SecurityToken.Model;
 using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
 
 namespace AWS.MSK.Auth.Tests;
 
 public class AwsmskAuthTokenGeneratorAsyncTest
 {
-    private static List<string> SIGV4_KEYS = new() { "Action", "X-Amz-Expires", "X-Amz-Algorithm", "X-Amz-Date", "X-Amz-SignedHeaders", "X-Amz-Credential", "User-Agent", "X-Amz-Signature", "X-Amz-Security-Token" };
+    private static readonly HashSet<string> Sigv4Keys =
+    [
+        "Action", "X-Amz-Expires", "X-Amz-Algorithm", "X-Amz-Date", "X-Amz-SignedHeaders", "X-Amz-Credential",
+        "User-Agent", "X-Amz-Signature", "X-Amz-Security-Token"
+    ];
 
-    private static AWSCredentials sessionCredentials = new SessionAWSCredentials("accessKey", "secretKey", "sessionToken");
+    private static readonly AWSCredentials SessionCredentials = new SessionAWSCredentials("accessKey", "secretKey", "sessionToken");
 
     [Fact]
-    public static async void GenerateAuthToken_TestNoCredentials()
+    public static async Task GenerateAuthToken_TestNoCredentials()
     {
-        List<FallbackCredentialsFactory.CredentialsGenerator> originalFallbackList = FallbackCredentialsFactory.CredentialsGenerators;
-
-        AWSMSKAuthTokenGenerator authTokenGeneratorAsync = new AWSMSKAuthTokenGenerator(null, NullLoggerFactory.Instance);
+        var originalFallbackList = AWSConfigs.AWSCredentialsGenerators;
 
         try
         {
-            FallbackCredentialsFactory.Reset();
-            FallbackCredentialsFactory.CredentialsGenerators = new List<FallbackCredentialsFactory.CredentialsGenerator>()
-            {
-                () => { return sessionCredentials; }
-            };
+            AWSConfigs.AWSCredentialsGenerators = [() => SessionCredentials];
 
-            (String token, long expiryMs) = await authTokenGeneratorAsync.GenerateAuthTokenAsync(RegionEndpoint.USEast1);
-            validateTokenSignature(token, expiryMs);
+            (String token, long expiryMs) = await new AWSMSKAuthTokenGenerator(loggerFactory: NullLoggerFactory.Instance).GenerateAuthTokenAsync(RegionEndpoint.USEast1);
+            ValidateTokenSignature(token, expiryMs);
         }
         finally
         {
-            FallbackCredentialsFactory.Reset();
-            FallbackCredentialsFactory.CredentialsGenerators = originalFallbackList;
+            AWSConfigs.AWSCredentialsGenerators = originalFallbackList;
         }
     }
 
     [Fact]
-    public static async void GenerateAuthToken_TestInjectedCredentials()
+    public static async Task GenerateAuthToken_TestInjectedCredentials()
     {
-        AWSMSKAuthTokenGenerator authTokenGeneratorAsync = new AWSMSKAuthTokenGenerator();
+        (var token, long expiryMs) = await new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(() => SessionCredentials, RegionEndpoint.USEast1);
 
-        var credentialsProviderMock = new Moq.Mock<Func<AWSCredentials>>();
-        credentialsProviderMock.Setup(provider => provider.Invoke()).Returns(sessionCredentials);
-        (var token, long expiryMs) = await authTokenGeneratorAsync.GenerateAuthTokenFromCredentialsProvider(credentialsProviderMock.Object, RegionEndpoint.USEast1);
+        ValidateTokenSignature(token, expiryMs);
+    }
 
-        validateTokenSignature(token, expiryMs);
+    [Fact]
+    public static async Task GenerateAuthToken_TestInjectedCredentialsWithSoonExpiration()
+    {
+        DateTime now = DateTime.UtcNow;
+        TimeSpan ttl = TimeSpan.FromMinutes(5);
+
+        (var token, long expiryMs) =
+            await new AWSMSKAuthTokenGenerator(timeProvider: () => now).GenerateAuthTokenFromCredentialsProvider(
+                () => new SessionAWSCredentials("accessKey", "secretKey", "sessionToken") { Expiration = now + ttl }, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs, ttl);
+    }
+
+    [Fact]
+    public static async Task GenerateAuthToken_TestInjectedCredentialsWithLongExpiration()
+    {
+        (var token, long expiryMs) = await new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(
+            () => new SessionAWSCredentials("accessKey", "secretKey", "sessionToken") { Expiration = DateTime.UtcNow.AddHours(6) }, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs);
+    }
+
+    private class TestCredentials : RefreshingAWSCredentials
+    {
+        private readonly DateTime _expiration;
+
+        public TestCredentials(DateTime expiration)
+        {
+            PreemptExpiryTime = TimeSpan.FromMinutes(15);
+            _expiration = expiration;
+        }
+
+        protected override CredentialsRefreshState GenerateNewCredentials() =>
+            new(
+                new ImmutableCredentials("accessKey", "secretKey", "sessionToken", "account"),
+                _expiration);
+    }
+
+    [Fact]
+    public static async Task GenerateAuthToken_RefreshingAwsCredentials_TestInjectedCredentialsWithLongExpiration()
+    {
+        var credentials = new TestCredentials(DateTime.UtcNow.AddHours(6));
+
+        var mskAuthTokenGenerator = new AWSMSKAuthTokenGenerator();
+
+        // First store credentials
+        await mskAuthTokenGenerator.GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+        (var token, long expiryMs) = await mskAuthTokenGenerator
+            .GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs);
+    }
+
+    [Fact]
+    public static async Task GenerateAuthToken_RefreshingAwsCredentials_TestInjectedCredentialsNotExpiringSoon()
+    {
+        DateTime now = DateTime.UtcNow;
+
+        var credentials = new TestCredentials(now.AddMinutes(29));
+
+        var mskAuthTokenGenerator = new AWSMSKAuthTokenGenerator(timeProvider: () => now);
+
+        // First store credentials
+        await mskAuthTokenGenerator.GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+        (var token, long expiryMs) = await mskAuthTokenGenerator
+            .GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs);
+    }
+
+    [Fact]
+    public static async Task GenerateAuthToken_RefreshingAwsCredentials_TestInjectedCredentialsCloseToExpiring()
+    {
+        DateTime now = DateTime.UtcNow;
+
+        var credentials = new TestCredentials(now.AddMinutes(16));
+
+        var mskAuthTokenGenerator = new AWSMSKAuthTokenGenerator(timeProvider: () => now);
+
+        // First store credentials
+        await mskAuthTokenGenerator.GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+        (var token, long expiryMs) = await mskAuthTokenGenerator
+            .GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs, TimeSpan.FromMinutes(8.5));
+    }
+
+    [Fact]
+    public static async Task GenerateAuthToken_RefreshingAwsCredentials_TestInjectedCredentialsAlreadyExpired()
+    {
+        DateTime now = DateTime.UtcNow;
+
+        var credentials = new TestCredentials(now.AddMinutes(10));
+
+        var mskAuthTokenGenerator = new AWSMSKAuthTokenGenerator(timeProvider: () => now);
+
+        // First store credentials
+        await mskAuthTokenGenerator.GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+        (var token, long expiryMs) = await mskAuthTokenGenerator
+            .GenerateAuthTokenFromCredentialsProvider(() => credentials, RegionEndpoint.USEast1);
+
+        ValidateTokenSignature(token, expiryMs, TimeSpan.FromMinutes(10));
     }
 
     [Fact]
@@ -63,67 +160,61 @@ public class AwsmskAuthTokenGeneratorAsyncTest
         AssumeRoleResponse assumeRoleResponse = new AssumeRoleResponse();
         assumeRoleResponse.Credentials = new Credentials("accessKey", "secretKey", "sessionToken", DateTime.Now);
 
-        var stsClientMock = new Moq.Mock<AmazonSecurityTokenServiceClient>();
+        RegionEndpoint region = RegionEndpoint.USEast1;
+
+        var stsClientMock = new Mock<AmazonSecurityTokenServiceClient>(region);
 
         stsClientMock.Setup(m => m.AssumeRoleAsync(It.Is<AssumeRoleRequest>(r => r.RoleArn == "arn:aws:iam::123456789101:role/MSKRole" && r.RoleSessionName == "mySession"), default)).Returns(Task.FromResult(assumeRoleResponse));
 
-        AWSMSKAuthTokenGenerator authTokenGeneratorAsync = new AWSMSKAuthTokenGenerator(stsClientMock.Object, null);
+        (var token, long expiryMs) = await new AWSMSKAuthTokenGenerator(stsClientMock.Object).GenerateAuthTokenFromRoleAsync(region, "arn:aws:iam::123456789101:role/MSKRole", "mySession");
 
-        (var token, long expiryMs) = await authTokenGeneratorAsync.GenerateAuthTokenFromRoleAsync(RegionEndpoint.USEast1, "arn:aws:iam::123456789101:role/MSKRole", "mySession");
-
-        validateTokenSignature(token, expiryMs);
+        ValidateTokenSignature(token, expiryMs);
     }
 
     [Fact]
-    public static void GenerateAuthToken_NullCredentials_ThrowsArgumentException()
+    public static async Task GenerateAuthToken_NullCredentials_ThrowsArgumentException()
     {
-        Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(null, RegionEndpoint.USEast1).AsTask());
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(null!, RegionEndpoint.USEast1).AsTask());
+        Assert.Contains("credentialsProvider", exception.Message);
     }
 
     [Fact]
-    public static void GenerateAuthToken_NullRegion_ThrowsArgumentException()
+    public static async Task GenerateAuthToken_NullRegion_ThrowsArgumentException()
     {
-        var credentialsProviderMock = new Moq.Mock<Func<AWSCredentials>>();
-        credentialsProviderMock.Setup(provider => provider.Invoke()).Returns(sessionCredentials);
-
-        Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(credentialsProviderMock.Object, null).AsTask());
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(() => SessionCredentials, null!).AsTask());
+        Assert.Contains("region", exception.Message);
     }
 
     [Fact]
-    public static void GenerateAuthToken_NullCredentials_ThrowsArgumentNullException()
+    public static async Task GenerateAuthToken_NullCredentials_ThrowsArgumentNullException()
     {
-        var credentialsProviderMock = new Moq.Mock<Func<AWSCredentials>>();
-
-        AWSCredentials credentials = null;
-
-        credentialsProviderMock.Setup(proivder => proivder.Invoke()).Returns(credentials);
-
-        Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(credentialsProviderMock.Object, null).AsTask());
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => new AWSMSKAuthTokenGenerator().GenerateAuthTokenFromCredentialsProvider(() => null!, RegionEndpoint.USEast1).AsTask());
+        Assert.Contains("credentials", exception.Message);
     }
 
-    private static void validateTokenSignature(string token, long expiryMs)
+    private static void ValidateTokenSignature(string token, long expiryMs, TimeSpan? expectedTtl = null)
     {
         byte[] decoded = Decode(token);
 
         var parsedUrl = new Uri(Encoding.UTF8.GetString(decoded, 0, decoded.Length));
         var queryParams = HttpUtility.ParseQueryString(parsedUrl.Query);
-        string[] credentialsTokens = queryParams["X-Amz-Credential"].Split('/');
+        string[] credentialsTokens = queryParams["X-Amz-Credential"]!.Split('/');
 
         Assert.Equal("kafka.us-east-1.amazonaws.com", parsedUrl.Host);
         Assert.Equal("kafka-cluster:Connect", queryParams["Action"]);
         Assert.Equal("host", queryParams["X-Amz-SignedHeaders"]);
         Assert.Equal("AWS4-HMAC-SHA256", queryParams["X-Amz-Algorithm"]);
-        Assert.Equal("900", queryParams["X-Amz-Expires"]);
+        Assert.Equal(expectedTtl is not null ? expectedTtl.Value.TotalSeconds.ToString(CultureInfo.InvariantCulture) : "900", queryParams["X-Amz-Expires"]);
         Assert.Equal("accessKey", credentialsTokens[0]);
         Assert.Equal("us-east-1", credentialsTokens[2]);
         Assert.Equal("kafka-cluster", credentialsTokens[3]);
         Assert.Equal("aws4_request", credentialsTokens[4]);
         Assert.Equal("sessionToken", queryParams["X-Amz-Security-Token"]);
         Assert.Equal("aws-msk-iam-sasl-signer-net-" + SignerVersion.CurrentVersion, queryParams["User-Agent"]);
-        Assert.True(Regex.IsMatch(queryParams["X-Amz-Date"], "(\\d{4})(\\d{2})(\\d{2})T(\\d{2})(\\d{2})(\\d{2})Z", RegexOptions.None));
-        Assert.All(queryParams.AllKeys, key => SIGV4_KEYS.Contains(key));
+        Assert.True(Regex.IsMatch(queryParams["X-Amz-Date"]!, "(\\d{4})(\\d{2})(\\d{2})T(\\d{2})(\\d{2})(\\d{2})Z", RegexOptions.None));
+        Assert.All(queryParams.AllKeys, key => Assert.Contains(key!, Sigv4Keys));
 
-        long expectedExpiryMs = new DateTimeOffset(DateTime.ParseExact(queryParams["X-Amz-Date"], "yyyyMMddTHHmmssZ", CultureInfo.InvariantCulture).Add(TimeSpan.FromSeconds(900))).ToUnixTimeMilliseconds();
+        long expectedExpiryMs = new DateTimeOffset(DateTime.ParseExact(queryParams["X-Amz-Date"]!, "yyyyMMddTHHmmssZ", CultureInfo.InvariantCulture).Add(expectedTtl ?? TimeSpan.FromSeconds(900))).ToUnixTimeMilliseconds();
         Assert.Equal(expectedExpiryMs, expiryMs);
     }
 
